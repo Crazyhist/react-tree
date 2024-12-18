5d4a7e2b82fe58b5617fedeca2f9c390edd26d40
Boss <crazyhistorian2000@yandex.ru>
2024-12-18 20:47:21 +0300

    react-tree

---
+++ src/app/main.tsx
@@ -0,0 +1,33 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+
+const data = [
+	{ id: '1', name: 'Unread' },
+	{ id: '2', name: 'Threads' },
+	{
+		id: '3',
+		name: 'Chat Rooms',
+		children: [
+			{ id: 'c1', name: 'General' },
+			{ id: 'c2', name: 'Random' },
+			{ id: 'c3', name: 'Open Source Projects' },
+		],
+	},
+	{
+		id: '4',
+		name: 'Direct Messages',
+		children: [
+			{ id: 'd1', name: 'Alice' },
+			{ id: 'd2', name: 'Bob' },
+			{ id: 'd3', name: 'Charlie' },
+		],
+	},
+]
+
+createRoot(document.getElementById('root')!).render(
+	<StrictMode>
+		<App data={data} />
+	</StrictMode>
+)
